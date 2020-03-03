71d6dfe987253d06a60c338d5f9277490d6da7ab
Lis-Marciniak.Jedrzej <215799@edu.p.lodz.pl>
2020-03-03 18:37:14 +0100

    Lab0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList stringsList = new ArrayList<>(value.length());
+        for (int i = 0; i <= value.length(); i++) {
+            stringsList.add(value.substring(i));
+        }
+        return stringsList;
     }
 
 }
